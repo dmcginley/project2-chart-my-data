e54f0be2991435d62d20d02f3fe9109a197aa50f
dmcginley <mcginley.donn@gmail.com>
2022-03-10 13:25:27 +0000

    js created a names array in papa parse

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,68 @@
-// FIXME: fix Papa.parse integration
+// FIXME: fix Papa.parse integration, to select the data from the file at the moment it is hardcoded
+
+// initial test data columns
 const firstData = [];
 const secondData = [];
 const thirdData = [];
+
+// labels, determines the length of the chart
 const labels = [];
 
-const fileInput = document.querySelector("#txtFileUpload");
+// empty array to separate out the first row from the file (the names)
+const names = [];
 
+const fileInput = document.querySelector("#txtFileUpload");
+const tableHead = document.querySelector("#txtFileUpload");
+
+// FIXME: trying to get the first row, the names
+// tableHead.addEventListener("change", (e) => {
+//   Papa.parse(fileInput.files[0], {
+//     download: true,
+//     skipEmptyLines: true,
+//     header: false,
+//     complete: function (results) {
+//       // console.log(results.data[0]);
+//       for (i = 0; i < results.data.length; i++) {
+//         names.push(results.data.[i] );
+//         console.log(names);
+//       }
+//     },
+//   });
+// });
+
+// using preview: 1 option to get just the first row in the csv
+tableHead.addEventListener("change", (e) => {
+  Papa.parse(fileInput.files[0], {
+    download: true,
+    skipEmptyLines: true,
+    header: false,
+    preview: 1,
+    complete: function (nameResults) {
+      // console.log(nameResults);
+      names.push(nameResults.data);
+      console.log(names);
+    },
+  });
+});
+
+// parsing the data with papa parse
 fileInput.addEventListener("change", (e) => {
   Papa.parse(fileInput.files[0], {
     download: true,
     skipEmptyLines: true,
     header: true,
     complete: function (results) {
-      console.log(results.data[0].day);
+      // console.log(results.data[0]);
       for (i = 0; i < results.data.length; i++) {
         firstData.push(results.data[i].day);
         secondData.push(results.data[i].price1);
         thirdData.push(results.data[i].price2);
         labels.push(results.data[i].month);
       }
-      console.log([1]);
-      console.log(firstData);
-      console.log(secondData);
-      console.log(thirdData);
+      // console.log(results.data[2]);
+      // console.log(firstData);
+      // console.log(secondData);
+      // console.log(thirdData);
     },
   });
 });
@@ -39,9 +79,17 @@ function updateChart(label) {
   if (label === "thirdData") {
     myChart.data.datasets[0].data = thirdData;
   }
-  console.log(label);
+  // console.log(label);
   myChart.update();
 }
+
+// dropdown buttons
+let selection = document.querySelector("select");
+let result = document.querySelector("h3");
+
+selection.addEventListener("change", () => {
+  result.innerHTML = selection.options[selection.selectedIndex].text;
+});
 
 // chart
 const data = {
